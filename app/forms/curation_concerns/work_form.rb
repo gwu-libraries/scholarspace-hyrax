d7f867f72d55396fcc1164bda4d19f4f34ea6abf
kerchner <kerchner@gwu.edu>
2016-09-19 18:26:40 +0000

    Adds ordering of fields on work edit form. Note that prev commit also added field hints/help text

--- app/forms/curation_concerns/work_form.rb
+++ app/forms/curation_concerns/work_form.rb
@@ -3,8 +3,12 @@
 module CurationConcerns
   class WorkForm < Sufia::Forms::WorkForm
     self.model_class = ::Work
-    self.terms += [:resource_type, :gw_affiliation]
     self.required_fields = [:title, :resource_type, :creator, :rights]
+
+    self.terms = [:title, :creator, :gw_affiliation, :date_created,
+                  :resource_type, :rights, :description, :tag,
+                  :identifier, :contributor, :publisher, :language,
+                  :based_near, :related_url, :bibliographic_citation]
 
 #    def primary_terms
 #      [:title, :creator, :rights, :tag, :resource_type, :gw_affiliation]
