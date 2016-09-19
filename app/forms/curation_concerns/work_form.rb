827a672cbb361750729dbf7806b8a6e06582ea14
kerchner <kerchner@gwu.edu>
2016-09-19 17:56:32 +0000

    Adds back field labels, app-level labels, no-rights value, required fields. Fixes #15

--- app/forms/curation_concerns/work_form.rb
+++ app/forms/curation_concerns/work_form.rb
@@ -4,6 +4,15 @@ module CurationConcerns
   class WorkForm < Sufia::Forms::WorkForm
     self.model_class = ::Work
     self.terms += [:resource_type, :gw_affiliation]
+    self.required_fields = [:title, :resource_type, :creator, :rights]
+
+#    def primary_terms
+#      [:title, :creator, :rights, :tag, :resource_type, :gw_affiliation]
+#    end
+
+#    def secondary_terms
+#      terms - primary_terms
+#    end
 
   end
 end
