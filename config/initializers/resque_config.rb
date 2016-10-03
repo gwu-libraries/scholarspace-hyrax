d0620efb54f719bf8b237a4277de7b825406cd4f
Matt Mihalik <matt.mihalik@gmail.com>
2016-10-03 11:02:55 -0400

    Use ScholarSphere config

--- config/initializers/resque_config.rb
+++ config/initializers/resque_config.rb
@@ -1,5 +1,9 @@
-config = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
+# frozen_string_literal: true
+rails_env = ENV['RAILS_ENV'] || 'development'
+
+config = YAML.load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result)['resque'].with_indifferent_access
 Resque.redis = Redis.new(host: config[:host], port: config[:port], thread_safe: true)
 
-Resque.inline = Rails.env.test?
-Resque.redis.namespace = "#{Sufia.config.redis_namespace}:#{Rails.env}"
+Resque.inline = rails_env == 'test'
+Resque.redis.namespace = "#{Sufia.config.redis_namespace}:#{rails_env}"
+Resque.logger.level = Logger::INFO
