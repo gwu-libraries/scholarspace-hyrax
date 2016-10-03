ba4a8aae57b84697b032ba4f79d2daef931299e3
Matt Mihalik <matt.mihalik@gmail.com>
2016-10-03 11:02:15 -0400

    Use Scholarsphere config example

--- config/initializers/redis_config.rb
+++ config/initializers/redis_config.rb
@@ -1,35 +1,14 @@
+# frozen_string_literal: true
+require 'redis'
+config = YAML.load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
+
 if defined?(PhusionPassenger)
   PhusionPassenger.on_event(:starting_worker_process) do |forked|
-    # We're in smart spawning mode.
-    if forked
-      # Re-establish redis connection
-      require 'redis'
-      config = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
-
-      # The important two lines
-      $redis.client.disconnect if $redis
-      $redis = Redis.new(host: config[:host], port: config[:port], thread_safe: true) rescue nil
-      Resque.redis = $redis
-      Resque.redis.namespace = "#{Sufia.config.redis_namespace}:#{Rails.env}"
-      Resque.redis.client.reconnect if Resque.redis
-    end
+    next unless forked
+    # Re-establish redis connection
+    Redis.current.disconnect!
+    Redis.current = Redis.new(config.merge(thread_safe: true))
   end
 else
-  config = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
-  $redis = Redis.new(host: config[:host], port: config[:port], thread_safe: true) rescue nil
-  Resque.redis.namespace = "#{Sufia.config.redis_namespace}:#{Rails.env}"
-end
-
-
-# Code borrowed from Obie's Redis patterns talk at RailsConf'12
-Nest.class_eval do
-  def initialize(key, redis=$redis)
-    super(key.to_param)
-    @redis = redis
-  end
-
-  def [](key)
-    self.class.new("#{self}:#{key.to_param}", @redis)
-    Resque.redis.namespace = "#{Sufia.config.redis_namespace}:#{Rails.env}"
-  end
+  Redis.current = Redis.new(config.merge(thread_safe: true))
 end
