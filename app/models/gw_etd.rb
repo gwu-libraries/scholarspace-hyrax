66bf123bbe607f99ab99b5bde4015c8bcf5bca08
Dan Kerchner <kerchner@gwu.edu>
2017-06-21 21:06:14 +0000

    For now, making GwEtd a child of base class, not GwWork; was not able to save changes as child of GwWork

--- app/models/gw_etd.rb
+++ app/models/gw_etd.rb
@@ -1,13 +1,17 @@
 # Generated via
 #  `rails generate hyrax:work GwEtd`
-class GwEtd < GwWork
+class GwEtd < ActiveFedora::Base
   include ::Hyrax::WorkBehavior
   include ::Hyrax::BasicMetadata
   # Change this to restrict which works can be added as a child.
   # self.valid_child_concerns = []
-#  validates :title, presence: { message: 'Your work must have a title.' }
+  validates :title, presence: { message: 'Your work must have a title.' }
   
   self.human_readable_type = 'Electronic Thesis/Dissertation'
+
+  property :gw_affiliation, predicate: ::RDF::URI.new('http://scholarspace.library.gwu.edu/ns#gwaffiliation') do |index|
+    index.as :stored_searchable, :facetable
+  end
 
   property :degree, predicate: ::RDF::URI.new("http://scholarspace.library.gwu.edu/ns#degrees"), multiple: false do |index|
     index.as :stored_searchable, :facetable
--- app/presenters/gw_etd_presenter.rb
+++ app/presenters/gw_etd_presenter.rb
@@ -1,3 +1,4 @@
 class GwEtdPresenter < GwWorkPresenter
+  delegate :gw_affiliation, to: :solr_document
   delegate :degree, to: :solr_document
 end
