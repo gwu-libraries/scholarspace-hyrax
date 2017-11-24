618a1a2a2fbb4085c774e7a7cfc6dbe28c858153
Dan Kerchner <kerchner@gwu.edu>
2017-11-24 15:40:59 -0500

    Addes permanent URL, Fixes #58, although base URL is hard coded for now

--- app/presenters/gw_etd_presenter.rb
+++ app/presenters/gw_etd_presenter.rb
@@ -1,4 +1,8 @@
 class GwEtdPresenter < GwWorkPresenter
   delegate :gw_affiliation, to: :solr_document
   delegate :degree, :advisor, :committee_member, to: :solr_document
+
+  def permanent_url
+    "https://scholarspace-etd.library.gwu.edu/etd/#{id}"
+  end
 end
--- app/presenters/gw_work_presenter.rb
+++ app/presenters/gw_work_presenter.rb
@@ -1,3 +1,7 @@
 class GwWorkPresenter < Hyrax::WorkShowPresenter
   delegate :gw_affiliation, to: :solr_document
+
+  def permanent_url
+    "https://scholarspace-etd.library.gwu.edu/work/#{id}"
+  end
 end
