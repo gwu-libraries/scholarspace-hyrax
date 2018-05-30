643e6a7bb7204153c29483be75b7faba62fe326e
Dan Kerchner <kerchner@gwu.edu>
2018-05-30 22:56:50 +0000

    Moved presenters into Hyrax module

--- app/presenters/gw_etd_presenter.rb
+++
@@ -1,8 +0,0 @@
-class GwEtdPresenter < GwWorkPresenter
-  delegate :gw_affiliation, to: :solr_document
-  delegate :degree, :advisor, :committee_member, to: :solr_document
-
-  def permanent_url
-    Scholarspace::Application.config.permanent_url_base + "etd/#{id}"
-  end
-end
