30af38a94cb0e2d0af7c985a395b4923b2bf120a
Dan Kerchner <kerchner@gwu.edu>
2018-06-11 22:04:50 +0000

    Added permanent_url config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.permanent_url_base = "https://scholarspace-etds.library.gwu.edu/"
 end
