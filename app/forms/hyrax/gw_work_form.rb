e582aa031d350b7e5d060049997027c29db513c8
Dan Kerchner <kerchner@gwu.edu>
2018-06-21 01:28:12 +0000

    Makes rights_statement required. Fixes #106

--- app/forms/hyrax/gw_work_form.rb
+++ app/forms/hyrax/gw_work_form.rb
@@ -3,12 +3,12 @@
 module Hyrax
   class GwWorkForm < Hyrax::Forms::WorkForm
     self.model_class = ::GwWork
-    self.required_fields = [:title, :resource_type, :creator, :license]
+    self.required_fields = [:title, :resource_type, :creator, :license, :rights_statement]
     self.terms += [:resource_type, :gw_affiliation]
 
     def secondary_terms
       [:gw_affiliation, :date_created,
-       :description, :keyword, :rights_statement,
+       :description, :keyword, 
        :identifier, :contributor, :publisher, :language,
        :based_near, :related_url, :bibliographic_citation]
     end
