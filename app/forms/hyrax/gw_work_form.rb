039fef2305d0841471eb7020067e7a2f16377269
Dan Kerchner <kerchner@gwu.edu>
2019-03-12 15:41:50 -0400

    bibliographic_citation seems to be missing from the base Hyrax WorkForm

--- app/forms/hyrax/gw_work_form.rb
+++ app/forms/hyrax/gw_work_form.rb
@@ -4,7 +4,7 @@ module Hyrax
   class GwWorkForm < Hyrax::Forms::WorkForm
     self.model_class = ::GwWork
     self.required_fields = [:title, :resource_type, :creator, :license, :rights_statement]
-    self.terms += [:resource_type, :gw_affiliation]
+    self.terms += [:resource_type, :gw_affiliation, :bibliographic_citation]
 
     def secondary_terms
       [:gw_affiliation, :date_created,
