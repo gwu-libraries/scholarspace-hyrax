a73ef12f82a449868b9ecedc6592d421d49b45b5
Dan Kerchner <kerchner@gwu.edu>
2019-11-05 11:45:42 -0500

    Fixes #240

--- app/models/gw_etd.rb
+++ app/models/gw_etd.rb
@@ -8,8 +8,6 @@ class GwEtd < ActiveFedora::Base
 
   validates :title, presence: { message: 'Your work must have a title.' }
   
-  self.human_readable_type = 'Electronic Thesis/Dissertation'
-
   property :gw_affiliation, predicate: ::RDF::URI.new('http://scholarspace.library.gwu.edu/ns#gwaffiliation') do |index|
     index.as :stored_searchable, :facetable
   end
