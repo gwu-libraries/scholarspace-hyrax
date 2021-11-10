a7fc828050a78f34ff00bc90961a8627c9f370d7
Laura Wrubel <lwrubel@gwu.edu>
2021-11-10 14:05:07 +0000

    Removing commented out line.

--- db/migrate/20170509232218_add_missing_indices.mailboxer_engine.rb
+++ db/migrate/20170509232218_add_missing_indices.mailboxer_engine.rb
@@ -5,7 +5,6 @@ class AddMissingIndices < ActiveRecord::Migration[5.0]
     # characters limitation.
     add_index :mailboxer_conversation_opt_outs, [:unsubscriber_id, :unsubscriber_type],
       name: 'index_mailboxer_conversation_opt_outs_on_unsubscriber_id_type'
-    #add_index :mailboxer_conversation_opt_outs, :conversation_id
 
     add_index :mailboxer_notifications, :type
     add_index :mailboxer_notifications, [:sender_id, :sender_type]
