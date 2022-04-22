bc9f941507142aebb1a87a2f662c69ce9e3ff305
dolsysmith <dsmith@gwu.edu>
2022-04-22 14:22:27 +0000

    Removed required fields not applicable

--- app/forms/hyrax/gw_journal_issue_form.rb
+++ app/forms/hyrax/gw_journal_issue_form.rb
@@ -4,11 +4,11 @@ module Hyrax
   # Generated form for GwJournalIssue
   class GwJournalIssueForm < GwWorkForm
     self.model_class = ::GwJournalIssue
-    self.required_fields = [:title, :creator, :license, :rights_statement]
-    self.terms += [:resource_type, :volume, :issue, :gw_affiliation, :doi, :bibliographic_citation]
+    self.required_fields = [:title]
+    self.terms += [:creator, :license, :rights_statement, :resource_type, :volume, :issue, :gw_affiliation, :doi, :bibliographic_citation]
 
     def secondary_terms
-      super + [:volume, :issue]
+      super + [:creator, :license, :rights_statement, :volume, :issue]
     end
   end
 end
