12c64eca51cbce3d812cf73039597cf2cd22e88f
Dan Kerchner <kerchner@users.noreply.github.com>
2022-12-14 09:25:42 -0500

    T360 recaptcha v3 (#362)
    Fixes #360; adds v3 reCAPTCHA; updates loofah
    
    Co-authored-by: Dolsy Smith <dsmith@gwu.edu>

--- app/controllers/hyrax/contact_form_controller.rb
+++ app/controllers/hyrax/contact_form_controller.rb
@@ -1,3 +1,4 @@
+require 'dotenv/load'
 module Hyrax
   class ContactFormController < ApplicationController
     before_action :build_contact_form
@@ -6,21 +7,35 @@ module Hyrax
     def new; end
 
     def create
-      # not spam and a valid form 
-      if verify_recaptcha
-        if @contact_form.valid?
-          ContactMailer.contact(@contact_form).deliver_now
-          flash.now[:notice] = 'Thank you for your message!'
-          after_deliver
-          @contact_form = ContactForm.new
-        else
-          flash.now[:error] = 'Sorry, this message was not sent successfully. '
-          flash.now[:error] << @contact_form.errors.full_messages.map(&:to_s).join(", ")
-        end
+      success = verify_recaptcha(action: 'contact', minimum_score: ENV['RECAPTCHA_MINIMUM_SCORE'].to_f, secret_key: ENV['RECAPTCHA_SECRET_KEY_V3'])
+      checkbox_success = verify_recaptcha unless success
+      if success || checkbox_success
+        ContactMailer.contact(@contact_form).deliver_now
+        flash.now[:notice] = 'Thank you for your message!'
+        after_deliver
+        @contact_form = ContactForm.new
       else
-        flash.now[:error] = 'Error passing CAPTCHA'
+        if !success
+          @show_checkbox_recaptcha = true
+        end
+        render :new
       end
-      render :new
+#    def create
+#      # not spam and a valid form 
+#      if verify_recaptcha
+#        if @contact_form.valid?
+#          ContactMailer.contact(@contact_form).deliver_now
+#          flash.now[:notice] = 'Thank you for your message!'
+#          after_deliver
+#          @contact_form = ContactForm.new
+#        else
+#          flash.now[:error] = 'Sorry, this message was not sent successfully. '
+#          flash.now[:error] << @contact_form.errors.full_messages.map(&:to_s).join(", ")
+#        end
+#      else
+#        flash.now[:error] = 'Error passing CAPTCHA'
+#      end
+#      render :new
     rescue RuntimeError => exception
       handle_create_exception(exception)
     end
