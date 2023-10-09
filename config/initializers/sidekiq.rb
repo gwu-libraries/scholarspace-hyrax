354d2f54a3a77191cc206c27ed519968af6eacd7
Matt Mihalik <matt.mihalik@gmail.com>
2023-10-09 10:25:37 -0400

    Update sidekiq.rb (#407)
    Ignore local SSL certs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 config = YAML.load(ERB.new(IO.read(Rails.root + 'config' + 'redis.yml')).result)[Rails.env].with_indifferent_access
 
-redis_conn = { url: "redis://#{config[:host]}:#{config[:port]}/" }
+redis_conn = { url: "redis://#{config[:host]}:#{config[:port]}/", ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 
 Sidekiq.configure_server do |s|
   s.redis = redis_conn
