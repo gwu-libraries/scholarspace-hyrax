19bd7492a7637beb05e3095d94d0ca5e0a99607e
Alex Boyd <alexanderleeboyd@gmail.com>
2023-12-25 21:29:19 -0500

    Add initial dashboard tests

---
+++ spec/features/dashboard_spec.rb
@@ -0,0 +1,81 @@
+require 'spec_helper'
+
+RSpec.describe "Dashboard page" do
+
+  it 'displays all admin controls when logged in as an admin user' do
+    admin_user = FactoryBot.create(:admin_user)
+
+    visit "/users/sign_in"
+
+    fill_in("user_email", with: admin_user.email)
+    fill_in("user_password", with: admin_user.password)
+    
+    click_button("Log in")
+
+    within ".sidebar" do
+      expect(page).to have_content("Dashboard")
+      expect(page).to have_content("Your activity")
+      expect(page).to have_content("Profile")
+      expect(page).to have_content("Notifications")
+      expect(page).to have_content("Transfers")
+      expect(page).to have_content("Manage Proxies")
+
+      expect(page).to have_content("Statistics")
+
+      expect(page).to have_content("Collections")
+      expect(page).to have_content("Works")
+      expect(page).to have_content("Importers")
+      expect(page).to have_content("Exporters")
+
+      expect(page).to have_content("Review Submissions")
+      expect(page).to have_content("Manage Embargoes")
+      expect(page).to have_content("Manage Leases")
+
+      expect(page).to have_content("Appearance")
+      expect(page).to have_content("Collection Types")
+      expect(page).to have_content("Pages")
+      expect(page).to have_content("Content Blocks")
+      expect(page).to have_content("Features")
+
+      expect(page).to have_content("Workflow Roles")
+    end
+  end
+
+  it 'displays all content-admin controls when logged in as a content-admin user' do
+    content_admin_user = FactoryBot.create(:content_admin_user)
+
+    visit "/users/sign_in"
+
+    fill_in("user_email", with: content_admin_user.email)
+    fill_in("user_password", with: content_admin_user.password)
+    
+    click_button("Log in")
+
+    within ".sidebar" do
+      expect(page).to have_content("Dashboard")
+      expect(page).to have_content("Your activity")
+      expect(page).to have_content("Profile")
+      expect(page).to have_content("Notifications")
+      expect(page).to have_content("Transfers")
+      expect(page).to have_content("Manage Proxies")
+      expect(page).to have_content("Collections")
+      expect(page).to have_content("Works")
+      expect(page).to have_content("Importers")
+      expect(page).to have_content("Exporters")
+
+      expect(page).to_not have_content("Statistics")
+      expect(page).to_not have_content("Review Submissions")
+      expect(page).to_not have_content("Manage Embargoes")
+      expect(page).to_not have_content("Manage Leases")
+
+      expect(page).to_not have_content("Appearance")
+      expect(page).to_not have_content("Collection Types")
+      expect(page).to_not have_content("Pages")
+      expect(page).to_not have_content("Content Blocks")
+      expect(page).to_not have_content("Features")
+
+      expect(page).to_not have_content("Workflow Roles")
+    end
+  end
+
+end
