a8ddaa0ef26571e118ebdecb3bdffe2de0a6ec7a
Alex Boyd <alexanderleeboyd@gmail.com>
2024-01-10 18:44:58 +0000

    Add capybara and simplecov to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,13 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'capybara/rspec'
+require 'simplecov'
+SimpleCov.start 'rails'
+
 RSpec.configure do |config|
+
+  config.backtrace_exclusion_patterns = []
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
