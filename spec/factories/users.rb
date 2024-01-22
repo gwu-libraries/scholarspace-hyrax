7d8f2eff0398c099f141169a88c1ea51841c666f
Alex Boyd <alexanderleeboyd@gmail.com>
2024-01-22 19:33:08 +0000

    Add users factory

---
+++ spec/factories/users.rb
@@ -0,0 +1,20 @@
+FactoryBot.define do
+  factory :user do
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+
+    factory :admin_user do
+      after :create do |user|
+        admin_role = Role.find_or_create_by(name: 'admin')
+        admin_role.users << user
+      end
+    end
+
+    factory :content_admin_user do
+      after :create do |user|
+        content_admin_role = Role.find_or_create_by(name: 'content-admin')
+        content_admin_role.users << user
+      end
+    end
+  end
+end
