dd227e58752d78c4b0389203140bad744152d278
Alex Boyd <alexanderleeboyd@gmail.com>
2024-01-30 02:05:30 +0000

    Allow setting visibility in gw_work factory

--- spec/factories/gw_works.rb
+++ spec/factories/gw_works.rb
@@ -1,7 +1,16 @@
 FactoryBot.define do
   factory :gw_work do
+    transient do
+      visibility { "public" }
+    end
+
     id { Noid::Rails::Service.new.mint }
     title { [Faker::Book.title] }
-    visibility { Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC }
+
+    after :create do |work, options|
+      work.visibility = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC if options.visibility == "public"
+      work.visibility = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE if options.visibility == "private"
+      work.visibility = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED if options.visibility == "authenticated"
+    end
   end
 end
