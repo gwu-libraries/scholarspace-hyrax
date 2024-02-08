0a5e3ce6376db23f06c657c0ea68a52c26c285d9
Max Turer <max.turer@gwu.edu>
2024-02-08 23:09:12 +0000

    route and template created for intermediate share page

--- config/routes.rb
+++ config/routes.rb
@@ -52,3 +52,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+Hyrax::Engine.routes.draw do
+  get 'share' =>'pages#show', key: 'share'
+end
