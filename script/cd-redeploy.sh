28daa1f6b641a2c8602113cb31b0724dcbdf16a3
Alex Boyd <alexanderleeboyd@gmail.com>
2024-02-20 15:19:45 -0500

    Update cd-redeploy.sh
    Adding additional needed steps to the CD redeploy script - running DB migrations if necessary, and then relaunching Passenger after precompiling assets.

--- script/cd-redeploy.sh
+++ script/cd-redeploy.sh
@@ -23,9 +23,15 @@ echo "Restarting Docker containers"
 docker compose up -d
 # If we don't sleep, Docker can't find the user
 sleep 2
+# Database migrations
+echo "Running Database Migrations"
+docker exec -i --user scholarspace $(docker ps --filter name=app -q) bash -lc "bundle exec rails db:migrate RAILS_ENV=production"
 # Precompile Assets
 echo "Precompiling assets"
 docker exec -i --user scholarspace $(docker ps --filter name=app -q) bash -lc "bundle exec rails assets:precompile RAILS_ENV=production"
 # Run rake task to apply any changes to contentblocks used on homepage/about/other main pages
 echo "Applying contentblocks"
 docker exec -i --user scholarspace $(docker ps --filter name=app -q) bash -lc "bundle exec rails gwss:apply_contentblock_changes RAILS_ENV=production"
+# Restart Passenger
+echo "Restarting Passenger"
+docker exec -i --user scholarspace $(docker ps --filter name=app -q) bash -lc "passenger-config restart-app /"
