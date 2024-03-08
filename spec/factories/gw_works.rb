336513b042dd9fa5aa19c953a11b77cf90ad3a4e
Alex Boyd <alexanderleeboyd@gmail.com>
2024-03-08 17:13:13 +0000

    Refactor gw_works factory visibility options

--- spec/factories/gw_works.rb
+++ spec/factories/gw_works.rb
@@ -1,20 +1,16 @@
 FactoryBot.define do
   factory :gw_work do
+    transient do
+      visibility { "public" }
+    end
+
     id { Noid::Rails::Service.new.mint }
     title { [Faker::Book.title] }
-    visibility { Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC }
-  
-    factory :gw_only_work do
-      id { Noid::Rails::Service.new.mint }
-      title { [Faker::Book.title] }
-      visibility { Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED }
-    end
 
-    factory :gw_private_work do
-      id { Noid::Rails::Service.new.mint }
-      title { [Faker::Book.title] }
-      visibility { Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE }
+    after :create do |work, options|
+      work.visibility = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC if options.visibility == "public"
+      work.visibility = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE if options.visibility == "private"
+      work.visibility = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED if options.visibility == "authenticated"
     end
-
   end
 end
