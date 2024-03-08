de68b7e010e8e67ac478501acb799e98279eb3ba
Alex Boyd <alexanderleeboyd@gmail.com>
2024-03-08 21:26:06 +0000

    Add user transient option to gw_works

--- spec/factories/gw_works.rb
+++ spec/factories/gw_works.rb
@@ -1,11 +1,12 @@
 FactoryBot.define do
   factory :gw_work do
-    transient do
-      visibility { "public" }
-    end
-
     id { Noid::Rails::Service.new.mint }
     title { [Faker::Book.title] }
+
+    transient do
+      visibility { "public" }
+      user { nil }
+    end
 
     after :create do |work, options|
       work.visibility = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC if options.visibility == "public"
