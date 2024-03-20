814e16a8ec6be0c41d2edbbf9bcf7057b8af9711
Max Turer <max.turer@gwu.edu>
2024-03-20 15:50:14 +0000

    collections route created

--- config/routes.rb
+++ config/routes.rb
@@ -58,6 +58,7 @@ end
 
 Hyrax::Engine.routes.draw do
   get 'share' =>'pages#show', key: 'share'
+  get '/collections', to: 'collections#index', as: 'collections'
   # Redirects non-privileged users to the application homepage
   authenticate :user, lambda { |u| !u.admin? && !u.contentadmin?} do
     namespace :dashboard do
