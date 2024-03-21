de6edec709bf8ea7c82af2b6fcf354c56cce268a
Alex Boyd <alexanderleeboyd@gmail.com>
2024-03-21 20:14:37 +0000

    Add solr and fedora cleaners

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,9 @@ require_relative '../config/environment'
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require_relative "../spec/support/database_cleaner"
+require_relative "../spec/support/fedora_cleaner"
+require_relative "../spec/support/solr_cleaner"
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
