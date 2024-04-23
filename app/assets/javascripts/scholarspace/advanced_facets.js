9528a2d16a5442fd1805490c67a72ac0211aadce
Max Turer <max.turer@gwu.edu>
2024-04-23 19:59:45 +0000

    changed location of javascript

--- app/assets/javascripts/scholarspace/advanced_facets.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.advanced-search-facet-select').chosen({
-      allow_single_deselect: true,
-      max_shown_results: 10000,
-      no_results_text: 'No results matched',
-      width: '100%'
-    });
-  });
