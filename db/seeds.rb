6052c43e3c8d82abe0c01edc352c50077376b20f
Alex Boyd <alexanderleeboyd@gmail.com>
2024-08-13 18:45:52 +0000

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,118 +1,64 @@
-# reworking this to only create the necessary collections/roles/users for a minimally
-# instance, and will move the seeding of works into a rake task
+abort("The Rails environment is running in production mode!") if Rails.env.production?
 
-def create_admin_role
-  @admin_role = Role.find_or_create_by(name: 'admin')
+ActiveFedora::Cleaner.clean!
+
+# users
+admin_user = FactoryBot.create(:admin, email: "admin@example.com")
+content_admin_user = FactoryBot.create(:content_admin, email: "content_admin@example.com")
+non_admin_user = FactoryBot.create(:user, email: "nonadminuser@example.com")
+
+# admin sets and collection types
+default_admin_set_id = AdminSet.find_or_create_default_admin_set_id
+admin_set_collection_type = FactoryBot.create(:admin_set_collection_type)
+gw_etds_admin_set = FactoryBot.create(:admin_set, title: ["ETDs"])
+
+# collections
+5.times do
+  FactoryBot.create(:collection, user: admin_user)
 end
 
-def create_default_admin_user
-  @admin_user = User.create!(email: ENV['DEV_ADMIN_USER_EMAIL'], password: ENV['DEV_ADMIN_USER_PASSWORD'])
-  admin_role = Role.find_or_create_by(name: 'admin')
-  admin_role.users << @admin_user
+# works with specific configurations - will need to log in as admin to see them
+FactoryBot.create(:work_with_image_files, user: admin_user, title: ["A Work with an Image File"])
+FactoryBot.create(:embargoed_work, user: admin_user, title: ["An Embargoed Work"])
+FactoryBot.create(:work_with_one_file, user: admin_user, title: ["A Work with a file"])
+FactoryBot.create(:work_with_file_and_work, user: admin_user, title: ["A Work with a file and work"])
+FactoryBot.create(:work_with_files, user: admin_user, title: ["A Work with files"])
+FactoryBot.create(:work_with_one_child, user: admin_user, title: ["A Work with one child"])
+FactoryBot.create(:work_with_two_children, user: admin_user, title: ["A Work with two children"])
+
+# public etds with metadata
+5.times do |i|
+  FactoryBot.create(:public_work, 
+                    user: admin_user, 
+                    admin_set: gw_etds_admin_set,
+                    title: ["Test work with metadata #{i}"],
+                    gw_affiliation: ["Department of Testing"],
+                    resource_type: ["Research Paper"],
+                    creator: ["Professor Goodtests"],
+                    contributor: ["Assistant Badtests"],
+                    description: ["A work for testing with metadata"],
+                    abstract: ["Ey I'm abstracting here"],
+                    keyword: ["Testing", "Examining", "Prodding"],
+                    license: ["http://www.europeana.eu/portal/rights/rr-r.html"], 
+                    rights_notes: [], 
+                    rights_statement: ["http://rightsstatements.org/vocab/InC/1.0/"], 
+                    access_right: [], 
+                    publisher: ["A Pretty Cool Publisher"], 
+                    date_created: [rand(1900..2010).to_s], 
+                    subject: ["Automated Testing"], 
+                    language: ["English"]
+                    )
 end
 
-def create_default_admin_set
-  @default_admin_set = Hyrax::AdminSetCreateService.find_or_create_default_admin_set
-end
+# content blocks
+ContentBlock.find_or_create_by(name: "header_background_color").update!(value: "#FFFFFF")
+ContentBlock.find_or_create_by(name: "header_text_color").update!(value: "#444444")
+ContentBlock.find_or_create_by(name: "link_color").update!(value: "#28659A")
+ContentBlock.find_or_create_by(name: "footer_link_color").update!(value: "#FFFFFF")
+ContentBlock.find_or_create_by(name: "primary_button_background_color").update!(value: "#28659A")
+ContentBlock.find_or_create_by(name: "featured_researcher").update!(value: File.open("#{Rails.root}/spec/fixtures/content_blocks/featured_researcher.html").read)
+ContentBlock.find_or_create_by(name: "about_page").update!(value: File.open("#{Rails.root}/spec/fixtures/content_blocks/about_page.html").read)
+ContentBlock.find_or_create_by(name: "help_page").update!(value: File.open("#{Rails.root}/spec/fixtures/content_blocks/help_page.html").read)
+ContentBlock.find_or_create_by(name: "share_page").update!(value: File.open("#{Rails.root}/spec/fixtures/content_blocks/share_page.html").read)
 
-def create_collection_type(machine_id, options)
-  coltype = Hyrax::CollectionType.find_by_machine_id(machine_id)
-  return coltype if coltype.present?
-  default_options = {
-    nestable: false, discoverable: false, sharable: false, allow_multiple_membership: false,
-    require_membership: false, assigns_workflow: false, assigns_visibility: false,
-    participants: [{ agent_type: Hyrax::CollectionTypeParticipant::GROUP_TYPE, agent_id: ::Ability.admin_group_name, access: Hyrax::CollectionTypeParticipant::MANAGE_ACCESS },
-                   { agent_type: Hyrax::CollectionTypeParticipant::GROUP_TYPE, agent_id: ::Ability.registered_group_name, access: Hyrax::CollectionTypeParticipant::CREATE_ACCESS }]
-  }
-  final_options = default_options.merge(options.except(:title))
-  Hyrax::CollectionTypes::CreateService.create_collection_type(machine_id: machine_id, title: options[:title], options: final_options)
-end
-
-def create_admin_set_collection_type
-  admin_set_collection_type = Hyrax::CollectionType.find_or_create_admin_set_type
-end
-
-def create_user_collection_type
-  user_collection_type = Hyrax::CollectionType.find_or_create_default_collection_type
-end
-
-def create_public_collection(user, type_gid, id, options)
-  options[:visibility] = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
-  create_collection(user, type_gid, id, options)
-end
-
-def create_etds_admin_set
-  etds_admin_set = Hyrax::AdministrativeSet.new(title: ['ETDs'])
-  etds_admin_set = Hyrax.persister.save(resource: etds_admin_set)
-  creating_user = User.where(email: ENV['DEV_ADMIN_USER_EMAIL']).first
-  Hyrax::AdminSetCreateService.call!(admin_set: etds_admin_set, creating_user: creating_user)
-  etds_admin_set
-end
-
-def create_collection(user, type_gid, id, options)
-  col = Collection.where(id: id)
-  return col.first if col.present?
-  col = Collection.new(id: id)
-  col.attributes = options.except(:visibility)
-  col.apply_depositor_metadata(user.user_key)
-  col.collection_type_gid = type_gid
-  col.visibility = options[:visibility]
-  col.save
-  Hyrax::Collections::PermissionsCreateService.create_default(collection: col, creating_user: user)
-  col
-end
-
-
-def create_etd(user, id, options)
-  work = GwEtd.where(id: id)
-  return work.first if work.present?
-  actor = Hyrax::CurationConcern.actor
-  attributes_for_actor = options
-  work = GwEtd.new(id: id)
-  actor_environment = Hyrax::Actors::Environment.new(work, Ability.new(user), attributes_for_actor)
-  actor.create(actor_environment)
-  work
-end
-
-def create_public_etd(user, id, options)
-  options[:visibility] = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
-  create_etd(user, id, options)
-end
-
-def create_private_etd(user, id, options)
-  options[:visibility] = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE
-  create_etd(user, id, options)
-end
-
-def create_authenticated_etd(user, id, options)
-  options[:visibility] = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED
-  create_etd(user, id, options)
-end
-
-def create_content_admin_role
-  @content_admin_role = Role.find_or_create_by(name: 'content-admin')
-end
-
-def create_content_admin_user
-  @content_admin_user = User.create!(email: "content-admin@example.com", password: "password")
-  content_admin_role = Role.find_or_create_by(name: "content-admin")
-  content_admin_role.users << @content_admin_user
-end
-
-# --------------
-
-# -- Creating admin role and user
-create_admin_role
-create_default_admin_user
-
-# -- Creating default admin sets, admin set collection type, and user collection type --
-create_default_admin_set
-create_admin_set_collection_type
-create_user_collection_type
-
-# -- Creating the ETDs admin set
-@etds_admin_set = create_etds_admin_set
-
-# -- Creating a content-admin role and user
-create_content_admin_role
-create_content_admin_user
+# bundle exec rails db:drop ; bundle exec rails db:create ; bundle exec rails db:migrate ; bundle exec rails db:seed
